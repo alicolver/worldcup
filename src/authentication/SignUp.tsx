8da529234ea36971b137e3157a53dabab0ad965a
Luke Ely <le2417@ic.ac.uk>
2021-06-10 21:19:47 +0100

    Fix copyright spacing

--- src/authentication/SignUp.tsx
+++ src/authentication/SignUp.tsx
@@ -6,6 +6,7 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
@@ -202,7 +203,9 @@ export default function SignUp() {
             </Grid>
           </div>
         </div>
-        <Copyright />
+        <Box mt={8}>
+          <Copyright />
+        </Box>
       </Container>
     );
   }
