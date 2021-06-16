f0ad6a3fd774da2306f218a8a3cbc51d410cc13a
Luke Ely <le2417@ic.ac.uk>
2021-06-16 12:55:08 +0100

    Change some sw stuff

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import * as serviceWorker from './serviceworker/serviceWorker'
+import * as serviceWorker from './serviceworker/serviceWorkerRegistration'
 
 ReactDOM.render(
   <React.StrictMode>
