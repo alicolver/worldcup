5fac831b122a6d2327286aaad3fcad2a473accc4
Simon Archer <simon.james.archer@gmail.com>
2022-11-16 23:09:03 +0000

    Adding logout button

--- src/misc/Header.tsx
+++ src/misc/Header.tsx
@@ -1,27 +1,31 @@
-import { makeStyles } from "@material-ui/core";
-import qatar from '../img/qatar.png'
+import { Container, makeStyles } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
+import qatar from "../img/qatar.png";
 import { deleteJWT } from "../utils/Utils";
+import LogoutIcon from "@mui/icons-material/Logout";
 
 const useStyles = makeStyles({
   logo: {
-    height: '6vh',
-    display: 'block',
-    marginLeft: 'auto',
-    marginRight: 'auto'
+    height: "6vh",
+    display: "block",
+    marginLeft: "auto",
+    marginRight: "auto",
   },
   headerBar: {
-    marginBottom: '5vw',
-    position: 'fixed',
-    width: '100%',
-    background: 'linear-gradient(90deg, rgba(154,12,52,1) 0%, rgba(0,0,0,1) 100%)',
+    marginBottom: "5vw",
+    position: "fixed",
+    width: "100%",
+    background:
+      "linear-gradient(90deg, rgba(154,12,52,1) 0%, rgba(0,0,0,1) 100%)",
     top: 0,
     zIndex: 10,
-    boxShadow: 'rgba(0, 0, 0, 0.16) 0px 1px 4px'
-  }
+    boxShadow: "rgba(0, 0, 0, 0.16) 0px 1px 4px",
+  },
 });
 
 export default function Header() {
   const classes = useStyles()
+  const history = useHistory()
 
   function logout() {
     deleteJWT()
@@ -29,8 +33,17 @@ export default function Header() {
   }
 
   return (
-    <div className={classes.headerBar} onClick={logout} style={{ padding: "0.5rem" }}>
-      <img className={classes.logo} src={qatar} alt={'qatar 2022 logo'} />
-    </div>
-  )
+    <>
+      <Container
+        className={classes.headerBar}
+        onClick={() => history.push("/home")}
+        style={{ padding: "0.5rem", display: "flex", alignItems: "center" }}
+      >
+        <img className={classes.logo} src={qatar} alt={"qatar 2022 logo"} />
+        <LogoutIcon onClick={() => logout()}
+          sx={{ color: "#FFFFFF", position: "fixed", right: "15px" }}
+        />
+      </Container>
+    </>
+  );
 }
